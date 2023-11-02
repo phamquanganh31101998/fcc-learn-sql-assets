080734f83643da4d014634a39561b9a95ff13a54
Quang Anh <anh.pq@teko.vn>
2023-11-02 22:23:12 +0700

    insert

--- course/4-crud/exercises/2-create/code.sql
+++ course/4-crud/exercises/2-create/code.sql
@@ -1,4 +1,6 @@
 -- ?
+INSERT INTO users(id, name, age, country_code, username, password, is_admin)
+VALUES (1, 'David', 34, 'US', 'DavidDev', 'insertPractice', false);
 
 
 -- TEST SUITE, DON'T TOUCH BELOW THIS LINE --
